90f85e3035fb8334e5657ae4961512a66d77fb42
Oksana Melnyk <oks.melnuk@gmail.com>
2021-08-11 01:34:08 +0300

    add slick slider to premium-slider blocks

--- movie-reviews/assets/js/main.js
+++ movie-reviews/assets/js/main.js
@@ -5,6 +5,9 @@ $( document ).ready(function() {
 $('.slider').slick({
     dots: true,
     arrows: false
+    infinite: true, // бесконечная прокрутка слайдов
+    slidesToShow: 5, // показывать по 3 слайда
+    slidesToScroll: 3, // прокручивать по 3 слайда за раз
   });
 
 });
