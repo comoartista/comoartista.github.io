4076cb0184ed3b6160de842e84454ce41e24157d
Oksana Melnyk <oks.melnuk@gmail.com>
2021-08-11 23:49:40 +0300

    add spotlight-tab, add footer

--- movie-reviews/assets/js/main.js
+++ movie-reviews/assets/js/main.js
@@ -3,11 +3,27 @@ $( document ).ready(function() {
     //   SLICK SLIDER
 // ==================================================
 $('.slider').slick({
-    dots: false,
-    arrows: false
+    // dots: false,
+    arrows: false,
     infinite: true, // бесконечная прокрутка слайдов
-    slidesToShow: 5, // показывать по 3 слайда
-    slidesToScroll: 3, // прокручивать по 3 слайда за раз
+
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 6000,
+
+  });
+
+    //   TAB
+// ==================================================
+  $('.tab-list .tab__link').click(function(event) {
+    event.preventDefault();
+  
+    $('.active').removeClass('active');
+    $(this).addClass('active');
+  
+    var id = this.id;
+    $('.' + id).addClass('active');
   });
 
 });
