74616d146ca48903cf8b8f62cc5869ef8326dddb
Oksana Melnyk <oks.melnuk@gmail.com>
2022-04-02 11:10:21 +0200

    done 47

---
+++ js_petrychenko/theory/47_classes/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+class ColoreRectangleWithText extends Rectangle {
+   constructor(height, width, text, bgColor) {
+        super(height, width); 
+        this.text = text;
+        this.bgColor = bgColor;
+   }
+
+   showMyProps() {
+       console.log(`Text: ${this.text}, колір: ${this.bgColor}`);
+   }
+}
+
+const div = new ColoreRectangleWithText(25, 10, 'Hello', 'red');
+
+div.showMyProps();
+console.log(div.calcArea());
+
+// const square = new Rectangle(10, 10);
+// const long = new Rectangle(20, 10);
+
+// console.log(square.calcArea());
+// console.log(long.calcArea());
