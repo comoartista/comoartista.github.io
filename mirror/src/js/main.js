77ace468a65e385052ed01d129f6f4b287e13816
Oksana Melnyk <oks.melnuk@gmail.com>
2022-04-13 20:27:42 +0200

    add aurhors in mirror

--- mirror/src/js/main.js
+++ mirror/src/js/main.js
@@ -40,3 +40,37 @@ function burgerMenu() {
   }
   window.addEventListener('scroll', fixedNav)
   
+  const swiper = new Swiper('.swiper', {
+     slidesPerView: 1,
+    loop: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+  //   // Responsive breakpoints
+  //   breakpoints: {
+  //   // when window width is >= 320px
+  //   320: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 20
+  //   },
+  //   // when window width is >= 480px
+  //   480: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 30
+  //   },
+  // }
+  });
